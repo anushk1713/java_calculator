bfe4de88744dab432fa1f31cd154ee1867c635eb
anushk1713 <kanabaragianush@gmail.com>
2026-02-28 15:32:11 +0530

    Add Java calculator source code

---
+++ Calculator.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("1. Add");
+            System.out.println("2. Subtract");
+            System.out.println("3. Multiply");
+            System.out.println("4. Divide");
+            System.out.println("5. Exit");
+
+            int choice = sc.nextInt();
+
+            if(choice == 5) {
+                System.out.println("Exiting...");
+                break;
+            }
+
+            System.out.print("Enter first number: ");
+            double num1 = sc.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double num2 = sc.nextDouble();
+
+            switch(choice) {
+                case 1:
+                    System.out.println("Result: " + (num1 + num2));
+                    break;
+
+                case 2:
+                    System.out.println("Result: " + (num1 - num2));
+                    break;
+
+                case 3:
+                    System.out.println("Result: " + (num1 * num2));
+                    break;
+
+                case 4:
+                    if(num2 == 0) {
+                        System.out.println("Cannot divide by zero");
+                    } else {
+                        System.out.println("Result: " + (num1 / num2));
+                    }
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+}
